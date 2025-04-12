5b1f0946554e870611102ba96d1e8ce96a3f56a9
Fadakar85 <erfan85.parham94@gmail.com>
2025-04-12 03:09:20 -0700

    Fixed folder issue

--- sms_utils.py
+++ sms_utils.py
@@ -4,29 +4,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-USERNAME = os.getenv('PAYAMAK_USERNAME')
-PASSWORD = os.getenv('PAYAMAK_PASSWORD')
-FROM_NUMBER = os.getenv('PAYAMAK_FROM')
+USERNAME = os.getenv('PAYAMAK_USERNAME')  # شماره کاربری پنل
+API_KEY = os.getenv('PAYAMAK_API_KEY')    # کلید API که در پنل دیدی
+FROM_NUMBER = os.getenv('PAYAMAK_FROM')   # شماره خط فرستنده مثل 1000xxxx
 
 def send_sms(phone_number, message):
     url = "https://rest.payamak-panel.com/api/SendSMS/SendSMS"
     payload = {
         "username": USERNAME,
-        "password": PASSWORD,
+        "password": API_KEY,  # استفاده از APIKey به‌جای رمز عبور
         "to": phone_number,
         "from": FROM_NUMBER,
         "text": message,
         "isflash": False
     }
+
     print(f"🔍 شماره ارسالی به API: [{phone_number}]")
     try:
         response = requests.post(url, data=payload)
         response.raise_for_status()
 
         data = response.json()
-        print("📨 پاسخ API:", data)  # اینجا چاپ می‌کنه
+        print("📨 پاسخ API:", data)
 
-        # بررسی موفقیت پیامک
         if data.get("RetStatus") == 1:
             print("✅ پیامک با موفقیت ارسال شد.")
         else:
